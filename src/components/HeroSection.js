a794765958e1301e5a49d1faf6d7a3850b7655a3
HighRollerLV <ip19.r.dambitis@vtdt.edu.lv>
2024-02-02 23:19:57 +0200

    Image Remodule

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,42 +1,23 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ContactUsModal from './ContactUsModal';
 
 const HeroSection = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [videoError, setVideoError] = useState(false);
 
     const toggleModal = () => {
         setIsModalOpen(!isModalOpen);
     };
 
-    // Handling video error
-    const handleVideoError = () => {
-        setVideoError(true);
-    };
-
     return (
         <div className="relative">
-            {!videoError ? (
-                <video
-                    src="/videos/BackgroundVideo.mp4"
-                    autoPlay
-                    loop
-                    muted
-                    className="w-full h-auto min-h-screen object-cover"
-                    onError={handleVideoError}
-                />
-            ) : (
-                <div className="w-full h-auto min-h-screen bg-gray-200 flex justify-center items-center">
-                    <p className="text-xl text-gray-700">Video could not be loaded.</p>
-                </div>
-            )}
+            <img src="/images/HeroSection.jpg" alt="Interior" className="w-full h-auto min-h-[800px] object-cover"/>
 
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center p-4 flex flex-col items-center justify-center">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center p-4">
                 <img src="/images/Amarant.jpg" alt="Silmežs Logo"
-                     className="w-3/4 min-w-[300px] h-auto object-contain"/>
+                     className="mx-auto w-full min-w-[300px] h-auto min-h-[250px] object-contain"/>
 
                 <button
-                    className="mt-4 px-4 py-2 text-white rounded text-lg sm:text-xl md:text-2xl bg-transparent border-2 sm:border-3 md:border-4 border-white hover:border-customDark hover:bg-customDark transition duration-300 break-words max-w-full"
+                    className="mt-4 px-4 py-2 text-white rounded text-lg sm:text-xl md:text-2xl bg-transparent border-2 sm:border-3 md:border-4 border-white hover:border-customDark hover:bg-customDark ease-in-out duration-300 break-words max-w-full"
                     onClick={toggleModal}
                 >
                     Contact Us
